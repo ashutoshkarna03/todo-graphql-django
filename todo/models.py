db392d0469785f5e535c330d7f1a6ec58607020b
Ashutosh <ashutoshkarna@Ashutoshs-MacBook-Pro.local>
2021-08-26 12:24:41 +0545

    added mutations for create, update and delete

--- todo/models.py
+++ todo/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class ToDoList(models.Model):
     task = models.CharField(null=False, max_length=50024)
     created_by = models.IntegerField()
-    is_complete = models.BooleanField()  
-    date_created = models.DateField(auto_now_add=True)
+    is_complete = models.BooleanField(default=False)  
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.task
--- todo/schema.py
+++ todo/schema.py
@@ -10,6 +10,7 @@ class ToDoListType(DjangoObjectType):
             'created_by',
             'is_complete',
             'date_created',
+            'id'
         )  
 
 
@@ -20,5 +21,64 @@ class Query(graphene.ObjectType):
         # Querying a list
         return ToDoList.objects.all()
 
+class UpdateToDoList(graphene.Mutation):
+    class Arguments:
+        # Mutation to update a to do list 
+        task = graphene.String(required=True)
+        id = graphene.ID()
+        created_by = graphene.Int()
+        is_complete = graphene.Boolean()
 
-schema = graphene.Schema(query=Query)
+    to_do_list = graphene.Field(ToDoListType)
+
+    @classmethod
+    def mutate(cls, root, info, task, id, created_by, is_complete):
+        to_do = ToDoList.objects.get(pk=id)
+        to_do.task = task
+        to_do.created_by = created_by
+        to_do.is_complete = is_complete
+        to_do.save()
+        
+        return UpdateToDoList(to_do_list=to_do)
+
+class CreateToDoList(graphene.Mutation):
+    class Arguments:
+        # Mutation to create a todo list
+        task = graphene.String(required=True)
+        created_by = graphene.Int()
+        is_complete = graphene.Boolean()
+
+    # Class attributes define the response of the mutation
+    to_do = graphene.Field(ToDoListType)
+
+    @classmethod
+    def mutate(cls, root, info, task, created_by, is_complete):
+        todo = ToDoList()
+        todo.task = task
+        todo.created_by = created_by
+        todo.is_complete = is_complete
+        todo.save()
+        
+        return CreateToDoList(to_do=todo)
+
+class DeleteToDoList(graphene.Mutation):
+    class Arguments:
+        # Mutation to delete a to do list 
+        id = graphene.ID()
+
+    to_do_list = graphene.Field(ToDoListType)
+
+    @classmethod
+    def mutate(cls, root, info, id):
+        to_do = ToDoList.objects.get(pk=id)
+        to_do.delete()
+        
+        return DeleteToDoList(to_do_list=to_do)
+
+class Mutation(graphene.ObjectType):
+    update_todo_list = UpdateToDoList.Field()
+    create_todo_list = CreateToDoList.Field()
+    delete_todo_list = DeleteToDoList.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
